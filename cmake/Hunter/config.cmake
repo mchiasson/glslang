986edf27da2ab84ba2a375f4d9bbd471eb72e77f
Mathieu-Andre Chiasson <mchiasson@gmail.com>
2019-09-26 22:08:00 -0400

    Hunterized

---
+++ cmake/Hunter/config.cmake
@@ -0,0 +1,12 @@
+hunter_config(SPIRV-Headers
+    VERSION 1.5.1
+    URL https://github.com/KhronosGroup/SPIRV-Headers/archive/1.5.1.tar.gz
+    SHA1 77018bfe6cb1eceaf824e401dbd206660a25bf66
+    CMAKE_ARGS SPIRV_HEADERS_ENABLE_EXAMPLES=OFF
+)
+
+hunter_config(SPIRV-Tools
+    VERSION v2019.4-p0
+    URL https://github.com/mchiasson/SPIRV-Tools/archive/v2019.4-p0.tar.gz
+    SHA1 fde8521fd5c99166de934d7d7d65800940dfba93
+)
